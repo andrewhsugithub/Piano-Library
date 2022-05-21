a926018d179c6cff277384a87b69e7bad2e6b281
Andrew <andrew1mail@gmail.com>
2022-05-21 18:58:47 +0800

    <understanding of midi(1)>

---
+++ test.py
@@ -0,0 +1,10 @@
+﻿# https://medium.com/analytics-vidhya/convert-midi-file-to-numpy-array-in-python-7d00531890c
+import mido
+# mid = mido.MidiFile('etude_25_12_(c)unknown.mid', clip=True)
+mid = mido.MidiFile('nocturne_27_2_(c)inoue.mid', clip=True)
+for _ in range(5):
+    print(f"{_} : ")
+    print(mid.tracks[_])
+    print("************************************************************************************")
+# for m in mid.tracks[0][:20]:
+#     print(m)
