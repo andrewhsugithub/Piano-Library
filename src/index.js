2679857e06349dff523cd2e4d0906f21770aa91b
Andrew <andrew1mail@gmail.com>
2022-06-08 00:08:48 +0800

    cleanup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import "normalize.css";
+import App from "./app";
+
+render(
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
+);
