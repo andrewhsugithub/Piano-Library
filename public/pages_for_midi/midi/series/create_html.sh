aba5faaf69c0ced778d4020cfd96cc435c3c4244
andrewhsugithub <100561529+andrewhsugithub@users.noreply.github.com>
2023-01-16 16:01:58 +0800

    Revert "Landing page"

--- public/pages_for_midi/midi/series/create_html.sh
+++
@@ -1,5 +0,0 @@
-﻿#!/bin/bash
-
-for name in $(cat main.txt); do
-    cat Attack_On_Titan.html > ${name}.html
-done
--- src/constants/routes.js
+++
@@ -1,5 +0,0 @@
-﻿export const HOME = "/";
-export const BROWSE = "/browse";
-export const SIGN_UP = "/signup";
-export const SIGN_IN = "/signin";
-export const MAIN = "/main";
